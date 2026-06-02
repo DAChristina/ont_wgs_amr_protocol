fd8fe4e5c7f7057badb588f4bf6566ab3c8d1ec7
DAChristina <chrisdewiress@gmail.com>
2026-06-02 08:43:28 +0700

    adjust df analyses result for viz

--- R/2_amr_plasmid_analyses.R
+++ R/2_amr_plasmid_analyses.R
@@ -15,111 +15,21 @@ source_all()
 # Batch 3 (xx May 2026)
 # Open new terminal and change owner to let the files be accessed with R
 
-current_batch <- 2
-patterns <- c(
-  "ncbi" = ".*\\_ncbi.tab$",
-  "resfinder" = ".*\\_resfinder.tab$",
-  "plasmidfinder" = ".*\\_plasmidfinder.tab$",
-  "vfdb" = ".*\\_vfdb.tab$"
-)
-
-for (db_name in names(patterns)) {
-  compiled <- compile_tabfiles(
-    path = paste0("inputs/output_abricate_batch", current_batch),
-    name_file = patterns[[db_name]]
-  )
-  
-  write.table(
-    compiled,
-    file = paste0("inputs/output_abricate_batch", current_batch,
-                       "/abricate_", db_name,"_batch", current_batch, 
-                       "_long.tsv"),
-    sep = "\t",
-    row.names = FALSE,
-    quote = FALSE
-  )
-}
-
-
-################################################################################
-# correct ID following workLab workSeq; combine batches and import to inputs/
-workLab_workSeq_all <- read.csv("inputs/workLab_workSeq_compiled_all.csv") %>% 
-  dplyr::select(id,
-                workDmx_filename,
-                workSeq_Speciator.genusName_post,
-                workSeq_Speciator.speciesName_post
-                ) %>% 
-  glimpse()
-
-abricate_fol <- list.dirs("inputs",
-                          recursive = FALSE,
-                          full.names = TRUE)
-abricate_fol <- abricate_fol[grepl("output_abricate_batch",
-                                   abricate_fol)]
-
-for (ft in c("long", "wide")){
-  patterns <- c(
-    "ncbi" = paste0(".*_ncbi_batch[0-9]+_", ft, "\\.t.*$"),
-    "resfinder" = paste0(".*_resfinder_batch[0-9]+_", ft, "\\.t.*$"),
-    "plasmidfinder" = paste0(".*_plasmidfinder_batch[0-9]+_", ft, "\\.t.*$"),
-    "vfdb" = paste0(".*_vfdb_batch[0-9]+_", ft, "\\.t.*$")
-  )
-  
-  for (db_name in names(patterns)){
-    compiled <- compile_tabfiles(
-      path = abricate_fol,
-      name_file = patterns[[db_name]]
-    ) %>% 
-      # delete info in the last "/" & weird extention
-      dplyr::mutate(
-        filename = gsub(".*/|\\.fastq.gz.long.fasta$|*_abricate_.*$||\\.tab$", "",
-                        X.FILE)
-      ) %>% 
-      dplyr::left_join(
-        workLab_workSeq_all
-        ,
-        by = c("filename" = "workDmx_filename")
-      ) %>% 
-      dplyr::rename_with(~ paste0("workAbr_", db_name, "_", .))
-    
-    write.table(
-      compiled,
-      file = paste0("inputs/abricate_", db_name,"_compiled_", ft, "_all.tsv"),
-      sep = "\t",
-      row.names = FALSE,
-      quote = FALSE
-    )
-  }
-}
-
-# I don't think full data acquired for "_wide" data
-# (bind_rows with possibly different columns)
-# How did Seeman compile the info to wide format again?
-# nah it is perfectly combined ;)
-
-test_wide <- read.table(
-  "inputs/abricate_plasmidfinder_compiled_wide_all.tsv",
-  header = TRUE,
-  sep = "\t",
-) %>% 
-  glimpse()
-
-
 ################################################################################
 # Analyse both acquired & point mutation-related AMR using abriTAMR
+# ALL data simultaneously (not per-batch)
 # This samplesheet can also be used for species-specific MLST scheme
 # Prepare samplesheet specified for species from all pass QC samples
-# Available MLST sheme option: mlst --info
-
+# Available MLST scheme option: mlst --info
 workLab_workSeq_all <- read.csv("inputs/workLab_workSeq_compiled_all.csv") %>% 
   dplyr::transmute(#id = id,
-                   #workDmx_filename = workDmx_filename,
-                   workAMR_fasta = gsub("output_demux", "output_ghru-mod_lambda_filter_on/assemblies",
-                                        workDmx_address),
-                   workAMR_fasta = gsub(".fastq", ".fastq.gz.long.fasta", workAMR_fasta),
-                   mlst_scheme = tolower(stringr::str_replace(workSeq_Speciator.speciesName_post,
-                                                      "(\\w)\\w+ (\\w+)", "\\1\\2")),
-                   abritamr_scheme = gsub(" ", "_", workSeq_Speciator.speciesName_post)
+    #workDmx_filename = workDmx_filename,
+    workAMR_fasta = gsub("output_demux", "output_ghru-mod_lambda_filter_on/assemblies",
+                         workDmx_address),
+    workAMR_fasta = gsub(".fastq", ".fastq.gz.long.fasta", workAMR_fasta),
+    mlst_scheme = tolower(stringr::str_replace(workSeq_Speciator.speciesName_post,
+                                               "(\\w)\\w+ (\\w+)", "\\1\\2")),
+    abritamr_scheme = gsub(" ", "_", workSeq_Speciator.speciesName_post)
   ) %>%
   # specify both MLST & abriTAMR scheme
   dplyr::mutate(
@@ -132,19 +42,142 @@ workLab_workSeq_all <- read.csv("inputs/workLab_workSeq_compiled_all.csv") %>%
   ) %>% 
   glimpse()
 
+# additional row for MLST scheme if required
 workLab_workSeq_all <- workLab_workSeq_all %>% 
   dplyr::filter(mlst_scheme == "ecoli") %>%
   dplyr::mutate(mlst_scheme = paste0(mlst_scheme, "_achtman_4")) %>%
   dplyr::bind_rows(workLab_workSeq_all, .) %>%
   dplyr::arrange(mlst_scheme) %>% 
+  
+  dplyr::filter(mlst_scheme == "abaumannii") %>%
+  dplyr::mutate(mlst_scheme = paste0(mlst_scheme, "_2")) %>%
+  dplyr::bind_rows(workLab_workSeq_all, .) %>%
+  dplyr::arrange(mlst_scheme) %>% 
   # view() %>% 
   glimpse()
 
+write.csv(workLab_workSeq_all, "inputs/species_samplesheet.csv",
+          row.names = FALSE, quote = FALSE)
 
-write.csv(workLab_workSeq_all,
-          "inputs/species_samplesheet.csv",
-          row.names = FALSE,
-          quote = FALSE
-          )
 
+
+################################################################################
+# compile abriTAMR results into one df
+workLab_workSeq_all <- read.csv("inputs/workLab_workSeq_compiled_all.csv") %>% 
+  dplyr::select(id,
+                workDmx_filename,
+                workSeq_Speciator.genusName_post,
+                workSeq_Speciator.speciesName_post
+  ) %>% 
+  glimpse()
+
+patterns <- c(
+  "abritamr" = ".*\\_abritamr.txt$",
+  # "amrfinder" = ".*\\_amrfinder.out$",
+  # amrfinder is special case coz' isolates not included in their output table
+  "summary_matches" = ".*\\_summary_matches.txt$",
+  "summary_partials" = ".*\\_summary_partials.txt$",
+  "summary_virulence" = ".*\\_summary_virulence.txt$"
+)
+
+for (db_name in names(patterns)) {
+  compiled <- compile_tabfiles(
+    path = paste0("inputs/output_abritamr/"),
+    name_file = patterns[[db_name]]
+  )
+  
+  write.table(
+    compiled,
+    file = paste0("inputs/abritamr_", db_name, ".tsv"),
+    sep = "\t",
+    row.names = FALSE,
+    quote = FALSE
+  )
+}
+
+# fixing amrfinder results
+files <- list.files("inputs/output_abritamr",
+                    pattern = ".*\\_amrfinder.out",
+                    full.names = TRUE,
+                    recursive = TRUE)
+
+compiled <- lapply(files, function(f) {
+  file_id <- basename(f) 
+  read.delim(f, sep = "\t", header = TRUE) %>% 
+    dplyr::mutate(
+      dplyr::across(everything(), as.character),
+      Class = stringr::str_to_title(Class),
+      Subclass = stringr::str_to_title(Subclass),
+      text_source = gsub("_amrfinder.out", "", file_id)
+    )
+}) %>% 
+  dplyr::bind_rows() %>% 
+  dplyr::mutate(
+    filename = text_source,
+    
+  ) %>%
+  dplyr::left_join(
+    workLab_workSeq_all
+    ,
+    by = c("filename" = "workDmx_filename")
+  ) %>% 
+  dplyr::rename_with(~ paste0("workAbr_", .))
+
+write.table(compiled, "inputs/abritamr_amrfinder.tsv",
+  sep = "\t",
+  row.names = FALSE,
+  quote = FALSE
+)
+
+# test AMR result
+amr <- read.table("inputs/abritamr_amrfinder.tsv",
+                  sep = "\t",
+                  header = TRUE,
+                  quote = "") %>% # use quote = "" to avoid tab misalignment
+  glimpse()
+
+
+################################################################################
+# compile ABRicate results into one df (long format)
+# Just use _long data for viz and further analyses; standardise the IDs
+# correct ID following workLab workSeq; combine batches and import to inputs/
+workLab_workSeq_all <- read.csv("inputs/workLab_workSeq_compiled_all.csv") %>% 
+  dplyr::select(id,
+                workDmx_filename,
+                workSeq_Speciator.genusName_post,
+                workSeq_Speciator.speciesName_post
+  ) %>% 
+  glimpse()
+
+patterns <- c(
+  "plasmidfinder" = ".*\\_plasmidfinder.tab$",
+  "vfdb" = ".*\\_vfdb.tab$"
+)
+
+for (db_name in names(patterns)) {
+  compiled <- compile_tabfiles(
+    path = paste0("inputs/output_abricate"),
+    name_file = patterns[[db_name]]
+  ) %>% 
+    dplyr::mutate(
+      filename = gsub(".*/|\\.fastq.gz.long.fasta$|*_abricate_.*$||\\.tab$", "",
+                      X.FILE),
+      gene_class = str_extract(PRODUCT, "(?<= - )[^\\(]+(?= \\()"),  # " - " & " ("
+      species_reference = str_extract(PRODUCT, "(?<=\\] \\[)[^\\]]+(?=\\])") # "] [" & "]"
+    ) %>%
+    dplyr::left_join(
+      workLab_workSeq_all
+      ,
+      by = c("filename" = "workDmx_filename")
+    ) %>% 
+    dplyr::rename_with(~ paste0("workAbr_", .))
+  
+  write.table(
+    compiled,
+    file = paste0("inputs/abricate_", db_name, "_long.tsv"),
+    sep = "\t",
+    row.names = FALSE,
+    quote = FALSE
+  )
+}
 
