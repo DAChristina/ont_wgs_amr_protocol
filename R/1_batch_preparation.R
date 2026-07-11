9f2000a9f47f86d0a2c8cf4b7f466316d11ca0c5
DAChristina <chrisdewiress@gmail.com>
2026-07-11 10:19:22 +0700

    combine df with received isolates from RSCM

--- R/1_batch_preparation.R
+++ R/1_batch_preparation.R
@@ -49,12 +49,53 @@ batch_qc <- compile_qc_batch(
 # "inputs/worklab_workSeq_compiled_batch<batch>.csv"
 
 ################################################################################
-# compile batches by using bind_rows
-workLab_workSeq_all <- compile_csvfiles() %>% 
+# compile batches by using left_join & bind_rows
+compiled_labSeq <- compile_csvfiles() %>% 
+  glimpse()
+
+workLab_workSeq_all <- dplyr::left_join(
+  
+  # received isolates from RS (duplication filtered)
+  readxl::read_excel("raw_data/update acorn hai_isolasi DNA.xlsx",
+                     sheet = "per June 26",
+                     skip = 1) %>% 
+    dplyr::select(2:9) %>% 
+    janitor::clean_names() %>% 
+    dplyr::rename_all(~ paste0("received_", .)) %>% 
+    # fix species name
+    dplyr::mutate(
+      received_isolate = case_when(
+        stringr::str_detect(received_isolate,
+                            regex("KPN|K\\.\\s*pneumoniae|Klebsiella\\s+pneumoniae",
+                                  ignore_case = TRUE)) ~ "Klebsiella pneumoniae",
+        stringr::str_detect(received_isolate,
+                            regex("E\\.\\s*coli|E\\.coli|Escherichia\\s+coli",
+                                  ignore_case = TRUE)) ~ "Escherichia coli",
+        stringr::str_detect(received_isolate,
+                            regex("A\\.\\s*baumannii|A\\.baumannii|A\\.\\s*baumanii|A\\.baumanii|Acinetobacter\\s+baumannii",
+                                  ignore_case = TRUE)) ~ "Acinetobacter baumannii",
+        stringr::str_detect(received_isolate,
+                            regex("P\\.\\s*aeruginosa|P\\.aeruginosa|P\\.\\s*aeroginosa|P\\.aeroginosa|Pseudomonas\\s+aeruginosa",
+                                  ignore_case = TRUE)) ~ "Pseudomonas aeruginosa",
+        TRUE ~ received_isolate
+      )
+    ) %>% 
+    dplyr::mutate(dplyr::across(everything(),
+                                as.character)) %>% 
+    dplyr::filter(!duplicated(
+      received_isolate_id,
+      fromLast = TRUE))
+  ,
+  # worklab_workSeq_compiled_batch
+  compiled_labSeq
+  ,
+  by = c("received_isolate_id" = "workLab_isolat_id")
+) %>% 
+  
   # generate personal id
   dplyr::mutate(
-    id = paste0(workLab_isolat_id, "_batch", workLab_batch, "_", workLab_native_barcode)
-  ) %>% 
+    id = paste0(received_isolate_id, "_batch", workLab_batch, "_", workLab_native_barcode)
+  ) %>%
   glimpse()
 
 write.csv(workLab_workSeq_all,
